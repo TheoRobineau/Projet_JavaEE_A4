f15e9c0f0d3dd21f4dd41376ef9349efe7bcf985
Laineux <theo.robineau@viacesi.fr>
2020-06-23 11:39:43 +0200

    Added Authentification service

---
+++ App/Backend/AuthService.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Backend
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "AuthService" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez AuthService.svc ou AuthService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class AuthService : IAuthService
+    {
+        public string login(string login, string password, string tokenApp)
+        {
+            string login1 = "test";
+            string pwd = "1234";
+            string token = "test";
+
+            string success = "Successfully connected";
+            string failure = "Failed to connected";
+
+            if (login == login1 && password == pwd && tokenApp == token)
+            {
+                return success;
+            }
+
+            return failure;
+        }
+    }
+}
--- App/Backend/IAuthService.cs
+++ App/Backend/IAuthService.cs
@@ -10,16 +10,13 @@ namespace Backend
 {
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
     [ServiceContract]
-    public interface IService1
+    public interface IAuthService
     {
 
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
 
         // TODO: ajoutez vos opérations de service ici
+        [OperationContract]
+        string login(string login, string password, string tokenApp);
     }
 
 
