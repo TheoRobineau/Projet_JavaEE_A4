54d3746c9629035125785e2ef17c4a578b1a9ba2
patri <patricia.hu@viacesi.fr>
2020-06-26 11:59:47 +0200

    Création des JMS Queue + méthode + message

--- App/JEE/messageReception/src/com/receptionmessage/ReceptionServiceBean.java
+++ App/JEE/messageReception/src/com/receptionmessage/ReceptionServiceBean.java
@@ -1,7 +1,14 @@
 package com.receptionmessage;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.jms.BytesMessage;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Queue;
 import javax.jws.WebService;
+import javax.inject.Inject;
+import java.io.UnsupportedEncodingException;
 
 
 @Stateless
@@ -12,8 +19,21 @@ import javax.jws.WebService;
 )
 public class ReceptionServiceBean implements ReceptionServiceBeanEndPointInterface{
 
-    public void receptMessage(byte[] file, byte[] key) {
+    @Inject
+    private JMSContext context;
 
-        return;
+    @Resource(lookup = "jms/messageQueue")
+    private Queue messageQueue;
+
+    public void receptMessage(byte[] file, byte[] key, byte[] fileName) throws JMSException, UnsupportedEncodingException {
+        String keyString = new String(key, "UTF-8");
+        String fileNameString = new String(fileName, "UTF-8");
+
+        BytesMessage bytesMessage = context.createBytesMessage();
+        bytesMessage.writeBytes(file);
+        bytesMessage.setStringProperty("key", keyString);
+        bytesMessage.setStringProperty("fileName", fileNameString);
+        context.createProducer().send(messageQueue,bytesMessage);
+        context.close();
     }
 }
--- App/JEE/messageReception/src/com/receptionmessage/ReceptionServiceBeanEndPointInterface.java
+++ App/JEE/messageReception/src/com/receptionmessage/ReceptionServiceBeanEndPointInterface.java
@@ -1,12 +1,14 @@
 package com.receptionmessage;
 
+import javax.jms.JMSException;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.io.UnsupportedEncodingException;
 
 
 @WebService(name = "ReceptionEndPoint")
-public interface ReceptionServiceBeanEndPointInterface{
+public interface ReceptionServiceBeanEndPointInterface {
     @WebMethod(operationName = "receptionOperation")
-    void receptMessage(@WebParam(name="file") byte[] file, @WebParam(name="key") byte[] key);
+    void receptMessage(@WebParam(name = "file") byte[] file, @WebParam(name = "key") byte[] key, @WebParam(name = "fileName") byte[] fileName) throws JMSException, UnsupportedEncodingException;
 }
