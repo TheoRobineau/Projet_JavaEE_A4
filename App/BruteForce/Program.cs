4a0b6ae5f502a1e2cf932b867da6bdcd60544d40
Dheev <dhev.zz@gmail.com>
2020-06-27 19:16:16 +0200

    brute force
    crée des threads en fonction du nombre de fichier

--- App/BruteForce/Program.cs
+++ App/BruteForce/Program.cs
@@ -1,24 +1,19 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
-
+using System.Threading.Tasks;
 namespace BruteForce
 {
     class Program
     {
-        // The same function is used to encrypt and 
-        // decrypt 
-        static String encryptDecrypt(String inputString, String key)
+        static String Decrypt(String inputString, String key)
         {
-            // variable permettant 
             int j = 0;
 
-            // string permetant de stocker la valeur de sorti
             String outputString = "";
 
-            // calcule le nombre de caractère dans le fichier txt
             int len = inputString.Length;
 
-            // Opération XOR sur tous les caractères du fichier txt
             for (int i = 0; i < len; i++)
             {
                 if (j == 4)
@@ -34,9 +29,14 @@ namespace BruteForce
             return outputString;
         }
 
-        static void decrypt1()
+        
+        static void decryptloop(Object args)
         {
-            string text2 = System.IO.File.ReadAllText(@"C:\Users\Dhev\Desktop\test\test2.txt");
+            Array argArray = new object[2];
+            argArray = (Array)args;
+            string text = (string)argArray.GetValue(0);
+            string compteur = (string)argArray.GetValue(1);
+            
 
             for (Char c1 = 'A'; c1 <= 'Z'; c1++)
             {
@@ -46,48 +46,39 @@ namespace BruteForce
                     {
                         for (Char c4 = 'A'; c4 <= 'Z'; c4++)
                         {
-                            encryptDecrypt(text2, "" + c1 + c2 + c3 + c4);
-                            if (encryptDecrypt(text2, "" + c1 + c2 + c3 + c4) == "bonjour")
+                            Decrypt(text, "" + c1 + c2 + c3 + c4);
+                           
+                            if ("" + c1 + c2 + c3 + c4 == "ZZZZ")
                             {
-                                Console.WriteLine("Threadd 2 finished at : " + DateTime.Now);
-                                Console.WriteLine("key : " + c1 + c2 + c3 + c4);
+                                Console.WriteLine("Thread " + compteur + " finished at : " + DateTime.Now);
                             }
                         }
                     }
                 }
             }
+
         }
+
 
         public static void Main(String[] args)
         {
             Console.WriteLine("Started at : " + DateTime.Now);
-            //Thread t = new Thread(new ThreadStart(decrypt1));
+            
+            string[] stringArray = new string[2];
 
-            //t.Start();
-
-
-            string text = System.IO.File.ReadAllText(@"C:\Users\Dhev\Desktop\test\test.txt");
-            //int len = text.Length;
-            for (Char c1 = 'A'; c1 <= 'Z'; c1++)
+            for (int i = 1; i < Directory.GetFiles(@"C:\Users\Dhev\Desktop\test").Length+1; i++)
             {
-                for (Char c2 = 'A'; c2 <= 'Z'; c2++)
-                {
-                    for (Char c3 = 'A'; c3 <= 'Z'; c3++)
-                    {
-                        for (Char c4 = 'A'; c4 <= 'Z'; c4++)
-                        {
-                            encryptDecrypt(text, "" + c1 + c2 + c3 + c4);
-                            if (encryptDecrypt(text, "" + c1 + c2 + c3 + c4) == "bonjour")
-                            {
-                                Console.WriteLine("Thread 1 finished at : " + DateTime.Now);
-                                Console.WriteLine("key : " + c1 + c2 + c3 + c4);
-                            }
-                        }
-                    }
-                }
+                stringArray[0] = File.ReadAllText(@"C:\Users\Dhev\Desktop\test\fichier" + i + ".txt");
+                stringArray[1] = ""+i;
+
+               
+                Thread t2 = new Thread(Program.decryptloop);
+                t2.Start(stringArray);
+                Thread.Sleep(10);
             }
 
 
+            
         }
     }
 }
