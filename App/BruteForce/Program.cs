d46688744c8f153ccc94161e6187705f97a2530e
Dheev <dhev.zz@gmail.com>
2020-06-28 01:29:01 +0200

    brute force

--- App/BruteForce/Program.cs
+++ App/BruteForce/Program.cs
@@ -1,43 +1,36 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 namespace BruteForce
 {
     class Program
     {
-        static String Decrypt(String inputString, String key)
+        
+        public string EncryptOrDecrypt(string text, string key)
         {
-            int j = 0;
+            var result = new StringBuilder();
 
-            String outputString = "";
+            for (int c = 0; c < text.Length; c++)
+                result.Append((char)((uint)text[c] ^ (uint)key[c % key.Length]));
 
-            int len = inputString.Length;
-
-            for (int i = 0; i < len; i++)
+            if (key == "ZZZZ")
             {
-                if (j == 4)
-                {
-                    j = 0;
-                }
-
-                outputString = outputString + char.ToString((char)(inputString[i] ^ key[j]));
-
-                j = j + 1;
-
+                System.Diagnostics.Debug.WriteLine(result.ToString());
             }
-            return outputString;
+            return result.ToString();
         }
 
-        
+
         static void decryptloop(Object args)
         {
             Array argArray = new object[2];
             argArray = (Array)args;
             string text = (string)argArray.GetValue(0);
             string compteur = (string)argArray.GetValue(1);
+            Program p = new Program();
             
-
             for (Char c1 = 'A'; c1 <= 'Z'; c1++)
             {
                 for (Char c2 = 'A'; c2 <= 'Z'; c2++)
@@ -46,8 +39,8 @@ namespace BruteForce
                     {
                         for (Char c4 = 'A'; c4 <= 'Z'; c4++)
                         {
-                            Decrypt(text, "" + c1 + c2 + c3 + c4);
-                           
+                            p.EncryptOrDecrypt(text, "" + c1 + c2 + c3 + c4);
+                            //Console.WriteLine("Thread : " + compteur +  "    " + c1 + c2 + c3 + c4);
                             if ("" + c1 + c2 + c3 + c4 == "ZZZZ")
                             {
                                 Console.WriteLine("Thread " + compteur + " finished at : " + DateTime.Now);
@@ -63,22 +56,22 @@ namespace BruteForce
         public static void Main(String[] args)
         {
             Console.WriteLine("Started at : " + DateTime.Now);
-            
+
             string[] stringArray = new string[2];
 
-            for (int i = 1; i < Directory.GetFiles(@"C:\Users\Dhev\Desktop\test").Length+1; i++)
+            for (int i = 1; i < Directory.GetFiles(@"C:\Users\Dhev\Desktop\test").Length + 1; i++)
             {
                 stringArray[0] = File.ReadAllText(@"C:\Users\Dhev\Desktop\test\fichier" + i + ".txt");
-                stringArray[1] = ""+i;
+                stringArray[1] = "" + i;
 
-               
+
                 Thread t2 = new Thread(Program.decryptloop);
                 t2.Start(stringArray);
                 Thread.Sleep(10);
             }
 
 
-            
+
         }
     }
 }
